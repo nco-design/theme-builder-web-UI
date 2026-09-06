d546cd39ef18672a5dbcbf0266b0e91cdec871f8
nco <coquinnicolas@gmail.com>
2026-09-06 17:20:12 +0800

    create template of the page

--- tools-data.js
+++ tools-data.js
@@ -5,5 +5,12 @@ window.toolCatalog = [
     description: "Create, preview and download color palettes for your themes.",
     path: "palette-generator/",
     version: "0.1.0"
+  },
+  {
+    id: "simple-project-generator",
+    name: "Simple Project Generator",
+    description: "Create and download a ready-to-edit Spruce Theme Builder project.",
+    path: "simple-project-generator/",
+    version: "0.1.0"
   }
 ];
